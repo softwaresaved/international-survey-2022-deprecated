66e8c4d6b602ed31af57bde9977f40c559e3b58f
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-01-20 22:30:05 +0000

    include/report: Autoformat using black

--- analysis/include/report.py
+++ analysis/include/report.py
@@ -24,16 +24,20 @@ def convert_time(x):
     except ValueError:
         return x
 
+
 def write_cache(name, data):
     cache_folder = Path("cache")
     if not cache_folder.exists():
         cache_folder.mkdir()
     data.to_csv(cache_folder / (name + ".csv"))
 
+
 def read_cache(name):
     cache_folder = Path("cache")
     if not cache_folder.exists():
-        raise ValueError("Cached item '%s' not found, run overview_and_sampling to generate")
+        raise ValueError(
+            "Cached item '%s' not found, run overview_and_sampling to generate"
+        )
     return pd.read_csv(cache_folder / (name + ".csv"))
 
 
@@ -41,7 +45,10 @@ def make_report(file):
     def inner_decorator(generator):
         def func(**kwargs):
             base = Path(file).stem
-            year = int(Path(file).resolve().parent.stem) or datetime.datetime.utcnow().year()
+            year = (
+                int(Path(file).resolve().parent.stem)
+                or datetime.datetime.utcnow().year()
+            )
             filename = base + ".md"
             template = first_existing(
                 [Path("templates") / filename, Path("../templates") / filename]
@@ -50,7 +57,9 @@ def make_report(file):
                 print("E: No template found for:", base)
                 print("   Put a corresponding template in the appropriate folder")
                 print("   For this year, in a 'template' subfolder of this folder")
-                print("   For all years, in a 'template' subfolder of the parent folder")
+                print(
+                    "   For all years, in a 'template' subfolder of the parent folder"
+                )
                 sys.exit(1)
 
             # Ensure these paths are present
@@ -62,14 +71,20 @@ def make_report(file):
             with template.open() as fp:
                 (Path("report") / (filename)).write_text(chevron.render(fp, report))
             return report
+
         return func
+
     return inner_decorator
 
 
 def table(name, data, index=True):
     csv = "csv/%s.csv" % name
     data.to_csv(csv, index=index)
-    return {"t_" + name: data.to_markdown(index=index) + "\n\n[Download CSV](%s)" % ("../" + csv)}
+    return {
+        "t_"
+        + name: data.to_markdown(index=index)
+        + "\n\n[Download CSV](%s)" % ("../" + csv)
+    }
 
 
 def figure(name, plt):
