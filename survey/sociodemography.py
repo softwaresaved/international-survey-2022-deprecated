c35fdccaed5534822de16c06660c3081b11f6c76
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-07-09 13:31:19 +0000

    Add ethnicity and disability data

--- survey/sociodemography.py
+++ survey/sociodemography.py
@@ -1,6 +1,7 @@
 # Sociodemography
 import sys
 import matplotlib
+import pandas as pd
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -29,20 +30,34 @@ age_order = [
 
 gender = ["socio2. Please select your gender"]
 
-ethn_us = ["socio5us. Do you consider yourself Hispanic or Latino"]
+ethn_us = ["socio5usqus._.Do you consider yourself Hispanic or Latino"]
 
 disability = [
-    "disa1. Do you have a condition that is defined as a disability by your country?"
+    "disa1._.Do you have a condition that is defined as a disability by your country?"
 ]
+
+
+def read_anonymised_data(survey_year, data, fillna='No'):
+    df = pd.read_csv(data).fillna(fillna)
+    df["socio1._.In which country do you work?"].replace(
+        {
+            "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+            "United States of America": "United States",
+        },
+        inplace=True,
+    )
+
+    df['Year'] = survey_year
+    return df.rename({'socio1._.In which country do you work?': 'Country'}, axis=1)
 
 
 @make_report(__file__)
 def run(survey_year, data="data/public_merged.csv"):
-    df = read_cache("processed_data")
-    ethnicity = [x for x in df.columns if x[:7] == "socio5."]
+    df = read_cache("processed_data").fillna('')
+    ethnicity_df = read_anonymised_data(survey_year, 'data/2018_ethnicity.csv')
+    disability_df = read_anonymised_data(survey_year, 'data/2018_disability.csv')
     countries = []
     for country in COUNTRIES_WITH_WORLD:
-        print(country)
         countries.append({"country": country})
         for category, columns, order_index in [
             ("Age", age, age_order),
@@ -56,27 +71,31 @@ def run(survey_year, data="data/public_merged.csv"):
                 results, country=country, category=category, order_index=order_index
             )
             countries[-1].update(figure_country(country, slugify(category), plt))
-        if country in ["United Kingdom", "United States"]:
+        if country == "United Kingdom":
+            category = 'Ethnicity'
             ethnicity_data = count_diff(
-                df, ethnicity, country, category, survey_year, multi_choice=True
+                ethnicity_df,
+                [x for x in ethnicity_df.columns if x.startswith('socio5quk')],
+                country, category, survey_year, multi_choice=True, disable_past_year=True
             )
             countries[-1].update(table_country(country, "ethnicity", ethnicity_data))
             plot_cat_comparison(results, country=country, category=category)
             countries[-1].update(figure_country(country, "ethnicity", plt))
-        if country == "United Kingdom":
+
             disability_data = count_diff(
-                df,
+                disability_df,
                 disability,
                 country,
                 "Officially recognised disability",
                 survey_year,
                 y_n=True,
+                disable_past_year=True
             )
             plot_cat_comparison(disability_data, country=country, category=category)
             countries[-1].update(figure_country(country, "disability", plt))
         if country == "United States":
             hispanic_latino_data = count_diff(
-                df, ethn_us, country, category, survey_year, y_n=True
+                ethnicity_df, ethn_us, country, category, survey_year, y_n=True
             )
             countries[-1].update(
                 table_country(country, "hispanic-or-latino", hispanic_latino_data)
