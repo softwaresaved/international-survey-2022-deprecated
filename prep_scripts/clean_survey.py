ee0811686bbbbd349efd0d6f74d7307ca3be8293
Steve Crouch <s.crouch@software.ac.uk>
2022-03-08 15:18:44 +0000

    Initial 2022 cleaning scripts

---
+++ prep_scripts/clean_survey.py
@@ -0,0 +1,85 @@
+"""Cleaning tool to do basic cleaning and amend the column names in a survey data CSV file to match the 2018 survey data."""
+
+import sys
+import re
+import pandas as pd
+
+# So we can load the survey and lib stuff
+sys.path.insert(1, '.')
+
+from survey.overview_and_sampling import read_salary
+from survey.sociodemography import read_anonymised_data
+
+
+# Pre-cleaning
+with open('cache/2022-precleaned.csv', 'w') as fout:
+    with open('data/2022-raw.csv', 'r') as fin:
+        for line in fin:
+            line = line.replace('&amp;', '&')
+            fout.write(line)
+
+# The precleaned new (2022) survey data
+fix_df = pd.read_csv('cache/2022-precleaned.csv', dtype=str, encoding='utf-8')
+fix_df['Year'] = 2022
+
+# The reference (2018) data to use
+# We need to assemble it from its different parts
+ref_df = pd.read_csv('data/2018.csv', encoding='utf-8')
+ref_df = ref_df.merge(read_salary('data/2018_salary.csv'), on='startdate. Date started')
+
+
+def search_replace_column_name(fix_df, col, ref_df, arg):
+    ref_cols = [c for c in ref_df.columns if re.match('^' + arg, c)]
+    if len(ref_cols) == 0:
+        print("**** Zero column matches on replace", arg, len(ref_cols), ref_cols[:2])
+        sys.exit(1)
+    elif len(ref_cols) > 1:
+        print("**** Multiple column matches on replace", arg, len(ref_cols), ref_cols[:2])
+        sys.exit(1)
+    print("Replacing", col, "with", ref_cols[0][:30] + '...')
+    fix_df.columns = [ref_cols[0] if c.startswith(col + '.') else c for c in fix_df.columns]
+
+
+def rename_column_name(fix_df, col, arg):
+    col_to_rename = [c for c in fix_df.columns if c.startswith(col)]
+    if len(col_to_rename) == 0:
+        print("**** Couldn't find column to rename", col)
+        sys.exit(1)
+    print("Renaming", col, "with", arg + '...')
+    fix_df.rename(columns={col_to_rename[0]: arg}, inplace=True)
+
+
+def delete_column(fix_df, col):
+    col_to_delete = [c for c in fix_df.columns if c.startswith(col)]
+    if len(col_to_delete) == 1:
+        print("Deleting", col)
+        fix_df.drop(columns=[col_to_delete[0]], inplace=True)
+    else:
+        print("**** Incorrect column matching:", col, col_to_delete)
+        sys.exit()
+
+
+# Column processing
+# Go through mapping CSV which indicates what to do with each 2022 column
+# to make the 2022 data compatible with the 2018 data
+# For each 2022 column, this could be Delete the column, Ignore it, a direct Rename, or SearchReplace it
+# which renames the column to match one in the 2018 data
+mapping_df = pd.read_csv('prep_scripts/survey_column_mapping.csv')
+for _, row in mapping_df.iterrows():
+    column = row.loc['QID_2022']
+    action = row.loc['Action']
+    argument = row.loc['Argument']
+
+    if action == 'SearchReplace':
+        # Uses a given pattern in the mapping CSV to identify a matching column name
+        # in the 2018 data to replace it with
+        search_replace_column_name(fix_df, column, ref_df, argument)
+    elif action == 'Rename':
+        rename_column_name(fix_df, column, argument)
+    elif action == 'Delete':
+        delete_column(fix_df, column)
+    elif action == 'Ignore':
+        pass
+
+
+fix_df.to_csv('data/2022.csv', index=False)
